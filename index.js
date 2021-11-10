1351dc56d6ba5efddff6d13fbb63fe3cb774625d
ghost <imransardar122@gmail.com>
2021-11-10 23:58:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const port = process.env.PORT || 5000
+
+//configure midleware cors
+app.use(cors());
+app.use(express.json());
+//single data loading by objectId
+const ObjectId = require('mongodb').ObjectId;
+//connect to db
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uvy8c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('IHandiCraftDB');
+        const productsCollection = database.collection('ProductsList');
+        //colection for insert place order data
+
+
+
+
+        //GET APi to get data
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products)
+        });
+
+
+
+
+
+        console.log('db connected');
+    } finally {
+
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
